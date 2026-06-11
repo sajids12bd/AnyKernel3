8ec3379be9413d5b6dfb33daaf0cd61c2fc5e75d
sajids12bd <59684420+sajids12bd@users.noreply.github.com>
2026-06-11 15:51:54 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,6 +1,5 @@
 ### AnyKernel3 Ramdisk Mod Script
 ## osm0sis @ xda-developers
-
 ### AnyKernel setup
 # global properties
 properties() { '
@@ -20,8 +19,6 @@ supported.versions=13-16
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
-
-
 ### AnyKernel install
 ## boot shell variables
 block=boot
@@ -29,71 +26,26 @@ is_slot_device=auto
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
 no_magisk_check=1
-
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
-
-# Kernel selection function
-choose_kernel() {
+# Flash kernel directly without selection menu
+if [ -f "$AKHOME/Image.clo.ksu" ]; then
   ui_print " "
-  ui_print "Kernel Version Selection:"
-  ui_print " "
-  ui_print "  VOL + : non-KSU"
-  ui_print "  VOL - : KSU"
-  ui_print " "
-  ui_print "Waiting for input... "
-  ui_print " "
-  ui_print " "
-
-  while true; do
-    input=$(getevent -qlc 1 2>/dev/null | grep -E "KEY_VOLUME(UP|DOWN)")
-    case "$input" in
-      *KEY_VOLUMEUP*)
-        return 1
-        ;;
-      *KEY_VOLUMEDOWN*)
-        return 2
-        ;;
-    esac
-    sleep 0.1
-  done
-}
-
-# Handle kernel selection
-if [ -f "$AKHOME/Image.ksu" ] && [ -f "$AKHOME/Image.noksu" ]; then
-  choose_kernel
-  case $? in
-    1)
-      ui_print " "
-      ui_print "Selected: non-KSU Kernel"
-      mv -f "$AKHOME/Image.noksu" "$AKHOME/Image"
-      ;;
-    2)
-      ui_print " "
-      ui_print "Selected: KSU Kernel"
-      mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
-      ;;
-  esac
+  ui_print "Flashing YASK CLO KSU Kernel..."
+  mv -f "$AKHOME/Image.clo.ksu" "$AKHOME/Image"
 elif [ -f "$AKHOME/Image" ]; then
   ui_print " "
-  ui_print "Single image kernel found, flashing it"
-  mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
-elif [ -f "$AKHOME/Image.ksu" ]; then
-  ui_print " "
-  ui_print "Only KernelSU version found, flashing it"
-  mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
-elif [ -f "$AKHOME/Image.noksu" ]; then
-  ui_print " "
-  ui_print "Only Standard version found, flashing it"
-  mv -f "$AKHOME/Image.noksu" "$AKHOME/Image"
+  ui_print "Flashing kernel..."
+else
+  ui_print "ERROR: No kernel image found!"
+  exit 1
 fi
-
 # boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
-    split_boot # for devices with init_boot ramdisk
-    flash_boot # for devices with init_boot ramdisk
+    split_boot
+    flash_boot
 else
-    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+    dump_boot
+    write_boot
 fi
 ## end boot install
